22b772cb602e3a670970e33b77ad852ebd9d0516
Joshua Flash <joshuaflash@gmail.com>
2021-12-19 20:16:55 +1300

    add UniTask to facilitate async thread management

--- Assets/Source/Battle/Phase.cs
+++ Assets/Source/Battle/Phase.cs
@@ -1,7 +1,6 @@
-using System.Collections.Generic;
-using System.Threading.Tasks;
+using System;
 
-public class Phase
+public class Phase : IEquatable<Phase>
 {
     private const int TOTAL_PHASES = 5;
     
@@ -16,10 +15,30 @@ public class Phase
     public static readonly Phase Burst = new (1);
     public static readonly Phase Spell = new (2);
     public static readonly Phase Ability = new (3);
-    public static readonly Phase Recovery = new (3);
+    public static readonly Phase Recovery = new (4);
 
     public static Phase operator ++(Phase phase)
     {
         return new Phase(phase.priority + 1);
+    }
+
+    public bool Equals(Phase other)
+    {
+        if (ReferenceEquals(null, other)) return false;
+        if (ReferenceEquals(this, other)) return true;
+        return priority == other.priority;
+    }
+
+    public override bool Equals(object obj)
+    {
+        if (ReferenceEquals(null, obj)) return false;
+        if (ReferenceEquals(this, obj)) return true;
+        if (obj.GetType() != this.GetType()) return false;
+        return Equals((Phase)obj);
+    }
+
+    public override int GetHashCode()
+    {
+        return priority;
     }
 }
