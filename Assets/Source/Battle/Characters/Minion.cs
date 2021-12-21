5971ef364f06a47d76841dfa0ef1e11fecc729e8
Joshua Flash <joshuaflash@gmail.com>
2021-12-21 18:45:09 +1300

    add work on minions and health

--- Assets/Source/Battle/Characters/Minion.cs
+++ Assets/Source/Battle/Characters/Minion.cs
@@ -1,5 +1,23 @@
+using System;
+using UnityEngine;
 
 public class Minion : Character
 {
+    [SerializeField] private HealthBar healthBar = default;
 
+    private void Start()
+    {
+        healthBar.SetMaxHealth(stats.MaxHealth);
+        healthBar.SetCurrentHealth(health);
+    }
+
+    private void OnEnable()
+    {
+        OnHealthChanged.AddListener(healthBar.SetCurrentHealth);
+    }
+    
+    private void OnDisable()
+    {
+        OnHealthChanged.RemoveListener(healthBar.SetCurrentHealth);
+    }
 }
