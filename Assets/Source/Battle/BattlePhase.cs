63925c7f2ada3508079c43f69fa62f33a7465fd1
Joshua Flash <joshuaflash@gmail.com>
2021-12-31 18:02:50 +1300

    change phase to record instead of class

--- Assets/Source/Battle/BattlePhase.cs
+++ Assets/Source/Battle/BattlePhase.cs
@@ -10,14 +10,5 @@ public static class BattlePhase
     public static readonly Phase Spell      = new (2, TOTAL_PHASES);
     public static readonly Phase Ability    = new (3, TOTAL_PHASES);
     public static readonly Phase Recovery   = new (4, TOTAL_PHASES);
-
-    public static IReadOnlyDictionary<Phase, string> BattlePhaseAliases = new Dictionary<Phase, string>
-    {
-        {Prep, "Prep"},
-        {Burst, "Burst"},
-        {Spell, "Spell"},
-        {Ability, "Ability"},
-        {Recovery, "Recovery"},
-    };
-
+    
 }
