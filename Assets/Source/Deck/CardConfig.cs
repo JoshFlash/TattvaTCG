7fe5540d2b080c6a2d0f0c1b5d89e3b95e5ebe3b
Joshua Flash <joshuaflash@gmail.com>
2022-01-03 15:11:09 +1300

    refactor a large portion of hand and card movement and control

--- Assets/Source/Deck/CardConfig.cs
+++ Assets/Source/Deck/CardConfig.cs
@@ -13,14 +13,9 @@ public class CardConfig : ScriptableObject
     public float DealtSpeed	= 0.21f;
     public float SortSpeed	= 0.14f;
 		
-    public Vector2 CardAreaPadding = new (0.14f, 0.28f);
-    public Vector2 MouseOverBounds = new (0.07f, 0.21f);
-
     public float SelectHeight   = 0.42f;
     public float SelectDepth    = -0.02f;
-    public float ExamineHeight  = 0.21f;
+    public float ExamineHeight  = 0.5f;
+    public float ExamineDepth  = -0.14f;
     public float DodgeDistance  = 0.07f;
-
-    public float ExamineDodgeDistance = 0.04f;
-    public float DodgeRightDistance = 0.04f;
 }
